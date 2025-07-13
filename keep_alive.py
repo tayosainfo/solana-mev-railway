642f0442230721a12cda9209c348c368a9ccec5d
tayosainfo <105611297+tayosainfo@users.noreply.github.com>
2025-07-13 13:37:11 +0300

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,1 +1,18 @@
 
+# keep_alive.py
+
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "✅ MEV Bot is running!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
